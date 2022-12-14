7de8b03ad56b8b03c7bb3c736a4e5595a81a3dd7
Kaitlyn Tracy <ktracy2@emich.edu>
2022-12-13 19:17:30 -0500

    Added check to while/dowhile

--- src/main/Validator.java
+++ src/main/Validator.java
@@ -804,6 +804,9 @@ public class Validator {
 				if(!isValidCodeBlock(codeBlock)) {
 					throw new ParserException("I don't know how you got here, so congratulations on that.");
 				}
+				if(!isValidSimpleStatement(codeBlock)) {
+					throw new ParserException("I don't know how you got here, so congratulations on that.");
+				}
 			}
 			else {
 				
@@ -850,6 +853,9 @@ public class Validator {
 			if (length > 2) { //valid code block
 				String codeBlock = doWhileLoop.substring(start, end).trim();
 				if(!isValidCodeBlock(codeBlock)) {
+					throw new ParserException("I don't know how you got here, so congratulations on that.");
+				}
+				if(!isValidSimpleStatement(codeBlock)) {
 					throw new ParserException("I don't know how you got here, so congratulations on that.");
 				}
 			}
